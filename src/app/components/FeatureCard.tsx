fc530f230c98ab756d86a93129a1adbd3e3177d5
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2025-04-20 19:49:46 -0500

    grid layout complete; pulling data from json file into FeatureCard component

---
+++ src/app/components/FeatureCard.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+
+export default function FeatureCard(card: object) {
+    return (
+        <div key={card.title} className="feature-card flex flex-col gap-3 items-center">
+            <Image
+              src={card.icon}
+              alt=""
+              width={50}
+              height={50}
+              className=""
+            />
+            <h3 className="font-semibold text-center">{card.title}</h3>
+            <p className="text-center text-neutral-600">{card.body}</p>
+          </div>
+    );
+}
